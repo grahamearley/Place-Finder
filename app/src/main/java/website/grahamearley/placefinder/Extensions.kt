ea7484132fff10d44111fc683516851a6588a108
Graham Earley <graham.p.earley@gmail.com>
2018-01-05 12:09:12 -0600

    Add extension functions for async API calls, view visibility

---
+++ app/src/main/java/website/grahamearley/placefinder/Extensions.kt
@@ -0,0 +1,33 @@
+package website.grahamearley.placefinder
+
+import android.opengl.Visibility
+import android.view.View
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+/**
+ * Created by grahamearley on 1/5/18.
+ */
+
+fun <T> Call<T>.enqueue(onResponse: (response: Response<T>?) -> Unit,
+                        onFailure: (throwable: Throwable?) -> Unit = {}) {
+    this.enqueue(object: Callback<T> {
+        override fun onFailure(call: Call<T>?, t: Throwable?) {
+            onFailure(t)
+        }
+
+        override fun onResponse(call: Call<T>?, response: Response<T>?) {
+            onResponse(response)
+        }
+
+    })
+}
+
+fun View.show() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    this.visibility = View.GONE
+}
