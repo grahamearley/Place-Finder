4346b4f2c48397752c72b004f27f69da50afb894
Graham Earley <graham.p.earley@gmail.com>
2018-01-05 16:23:46 -0600

    Add recycler adapter for venue items

---
+++ app/src/main/java/website/grahamearley/placefinder/VenueRecyclerAdapter.kt
@@ -0,0 +1,30 @@
+package website.grahamearley.placefinder
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+
+/**
+ * Adapter for displaying lists of Venue cards.
+ */
+class VenueRecyclerAdapter(private val venues: List<Venue>) : RecyclerView.Adapter<VenueViewHolder>() {
+
+    override fun getItemCount(): Int = venues.size
+
+    override fun onBindViewHolder(holder: VenueViewHolder?, position: Int) {
+        val venue = venues[position]
+        holder?.apply {
+            venueImageView.hide() // todo: show pics! with picasso
+
+            titleTextView.text = venue.name
+            addressTextView.text = venue.location.formattedAddress.firstOrNull()
+            categoryTextView.text = venue.categories.firstOrNull()?.name
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): VenueViewHolder {
+        val itemView = LayoutInflater.from(parent?.context).inflate(R.layout.list_item_venue, parent, false)
+        return VenueViewHolder(itemView)
+    }
+}
+
---
+++ app/src/main/java/website/grahamearley/placefinder/VenueViewHolder.kt
@@ -0,0 +1,14 @@
+package website.grahamearley.placefinder
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.list_item_venue.view.*
+
+class VenueViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val venueImageView: ImageView = itemView.venueImageView
+    val titleTextView: TextView = itemView.titleTextView
+    val addressTextView: TextView = itemView.addressTextView
+    val categoryTextView: TextView = itemView.categoryTextView
+}
