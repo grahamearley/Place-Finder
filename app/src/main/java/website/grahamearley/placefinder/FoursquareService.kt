943fe276cb18b12790cbc820609ec82cacb928d1
Graham Earley <graham.p.earley@gmail.com>
2018-01-05 10:02:26 -0600

    Update models and query for explore API call

--- app/src/main/java/website/grahamearley/placefinder/FoursquareService.kt
+++ app/src/main/java/website/grahamearley/placefinder/FoursquareService.kt
@@ -1,17 +1,19 @@
 package website.grahamearley.placefinder
 
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 /**
- * Created by grahamearley on 1/4/18.
+ * A service for requesting venues from the Foursquare API.
  */
 interface FoursquareService {
     @GET("venues/explore")
-    fun requestVenues(@Query("client_id") clientId: String,
-                      @Query("client_secret") clientSecret: String,
+    fun requestVenues(@Query("client_id") clientId: String = CLIENT_ID,
+                      @Query("client_secret") clientSecret: String = CLIENT_SECRET,
+                      @Query("v") version: String = API_VERSION,
                       @Query("section") section: String = "food",
                       @Query("near") near: String,
                       @Query("query") query: String,
-                      @Query("venuePhotos") venuePhotos: Int)
+                      @Query("venuePhotos") venuePhotos: Int): Call<FoursquareResponse>
 }
