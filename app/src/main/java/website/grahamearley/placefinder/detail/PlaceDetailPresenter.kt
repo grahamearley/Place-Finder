fd99439eaa813cf760b6ff2e010ff2ba94c27440
Graham Earley <graham.p.earley@gmail.com>
2018-01-08 13:07:52 -0600

    Add properties for accessing venue information in presenter

--- app/src/main/java/website/grahamearley/placefinder/detail/PlaceDetailPresenter.kt
+++ app/src/main/java/website/grahamearley/placefinder/detail/PlaceDetailPresenter.kt
@@ -10,6 +10,18 @@ import website.grahamearley.placefinder.detail.contract.PlaceDetailViewContract
 class PlaceDetailPresenter(override val view: PlaceDetailViewContract) : PlaceDetailPresenterContract {
     override var venueItem: VenueItem? = null
 
+    private val reason get() = venueItem?.reasons?.items?.firstOrNull()
+    private val images get() = venueItem?.venue?.getPhotoUrlsOrNull()
+    private val tips get() = venueItem?.tips
+    private val name get() = venueItem?.venue?.name
+    private val address get() = venueItem?.venue?.getStreetAddressOrNull()
+    private val category get() = venueItem?.venue?.getFirstCategoryOrNull()
+    private val rating get() = venueItem?.venue?.rating
+    private val ratingColor get() = venueItem?.venue?.ratingColor
+    private val menuUrl get() = venueItem?.venue?.menu?.mobileUrl
+    private val phoneNumber get() = venueItem?.venue?.contact?.phone
+    private val website get() = venueItem?.venue?.url
+
     override fun onViewCreated() {
 
     }
